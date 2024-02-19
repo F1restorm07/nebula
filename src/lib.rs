fbafe9adedcf510724234b659afec54c0ee5e59a
F1restorm <luke.n.gardner@icloud.com>
2024-02-18 23:30:13 -0700

    add visibitlity to crate items

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 #![no_std]
 
-extern crate alloc;
+extern crate alloc; // used for sequence construction
 
-pub mod colors;
-pub mod attributes;
 pub mod cursor;
 pub mod screen;
-pub mod raw;
-pub mod event;
-pub mod input;
 pub mod error;
+
+mod colors;
+mod attributes;
+mod raw;
+mod event;
+mod input;
+
+pub use crate::{
+    colors::*,
+    attributes::*,
+    raw::*, input::EventIterator,
+};
 
 #[macro_export]
 macro_rules! csi {
